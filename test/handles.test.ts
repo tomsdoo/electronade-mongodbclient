1ebba98dacd6923d5d34dd6a03b4fb93bc101bd3
tom <toms@live.jp>
2022-08-26 13:47:45 +0900

    test for electronade-mongodbclient:insertmany

--- test/handles.test.ts
+++ test/handles.test.ts
@@ -3,8 +3,24 @@ import { strict as assert } from "assert";
 
 import { handles } from "../src/";
 
+let handleStore: {
+  [key: string]: Function;
+};
+
 describe("handles", () => {
+  before(() => {
+    handleStore = Object.fromEntries(
+      handles.map(({ eventName, handler }) => [ eventName, handler ])
+    );
+  });
+
   it("handles are exported", () => {
     assert(handles);
+  });
+
+  it("electronade-mongodbclient:insertmany eventName exists", () => {
+    assert(
+      "electronade-mongodbclient:insertmany" in handleStore
+    );
   });
 });
