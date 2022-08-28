e07c55e464c4af75a6b5e19627875e922a117c66
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-08-28 09:07:13 +0900

    electronade-mongodbclient:read is added (#29)

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,23 @@ export const handles = [
         items: object[];
       }
     ) => new MClient(uri, db, collection).insertMany(items)
+  },
+  {
+    eventName: "electronade-mongodbclient:read",
+    handler: (
+      event: any,
+      {
+        uri,
+        db,
+        collection,
+        condition
+      }: {
+        uri: string;
+        db: string;
+        collection: string;
+        condition?: any;
+      }
+    ) => new MClient(uri, db, collection).read(condition)
   }
 ];
 
@@ -31,6 +48,15 @@ export const preloadObject = {
     ) => ipcRenderer.invoke(
       "electronade-mongodbclient:insertmany",
       { uri, db, collection, items }
+    ),
+    read: (
+      uri: string,
+      db: string,
+      collection: string,
+      condition?: any
+    ) => ipcRenderer.invoke(
+      "electronade-mongodbclient:read",
+      { uri, db, collection, condition }
     )
   }
 };
